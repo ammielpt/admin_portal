4472c6264f88e8a77dee314acb13db86364d131c
Ammiel Eliezer Pajuelo Tapia <apajuelot@atento.com.pe>
2020-01-23 18:18:44 -0500

    cambios al 23_01_2020

---
+++ database/migrations/2020_01_23_171341_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('rol_name')->nullable(); // administrador del sitio
+            $table->string('rol_key_name')->unique(); //admin            
+            $table->text('rol_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//\App\User::create([
+//    'name'=>'Ammiel'
+//]);
 Route::view('/logins', 'login')->name('login');
 Route::view('/main', 'index')->name('redirect');
 Route::view('/noticias', 'noticias.lista_noticia')->name('noticias');
 Route::view('/noticias/nuevo', 'noticias.form_nuevo_noticia')->name('noticia.nuevo');
 Route::view('/noticias/editar', 'noticias.form_edit_noticia')->name('noticia.editar');
+Route::get('usuarios', 'UsersController@index')->name('usuarios.index');
+Route::get('usuarios/{id}/editar', 'UsersController@edit')->name('usuarios.edit');
+Route::put('usuarios/{id}', 'UsersController@update')->name('usuarios.update');
+Route::post('usuarios/{id}', 'UsersController@store')->name('usuarios.store');
+Route::post('usuarios/{id}', 'UsersController@show')->name('usuarios.show');
+Route::delete('usuarios/{id}', 'UsersController@destroy')->name('usuarios.destroy');
 Auth::routes();
 Route::post('logout', function(){
     Auth::logout();
